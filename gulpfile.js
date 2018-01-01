f5623c31930b885914d68721fb3fd8ae0b21b3ff
salah belouad <devsalah101@gmail.com>
2018-01-01 22:22:19 +0000

    Modified

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,21 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const postcss = require('gulp-postcss');
 const uncss = require('postcss-uncss');
+const autoprefixer = require('autoprefixer');
 
 gulp.task("sass", () => {
 	const plugins = [
 		uncss({
 			html: ['index.html', '_includes/*.html', '_layouts/*.html'],
 			ignore: ['.fade']
+		}),
+		autoprefixer({
+			browsers: ['last 5 versions'],
+			casecase: false
 		})
 	];
+	return gulp.src("_sass/**/*.scss")
+	.pipe(postcss(plugins))
+	.pipe(gulp.dest("assets/css/"));
 
 });
